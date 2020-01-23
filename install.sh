fb1793ce9bcb63d5740d9362794744ebc0b987cb
mk <mk@mk.mk>
2020-01-23 22:50:47 +0000

    updates

--- install.sh
+++ install.sh
@@ -14,20 +14,16 @@ apt install -y apt-transport-https ca-certificates curl gnupg-agent software-pro
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-#apt-add-repository --yes --update ppa:ansible/ansible
 
 apt update
 apt install -y software-properties-common python-virtualenv python-pip
 apt install -y docker-ce docker-ce-cli containerd.io
-usermod -aG docker $USER
-#apt install -y ansible
+usermod -aG docker $executer
 
 # install ansible role
-ansible-galaxy install --roles-path ./roles emmetog.jenkins 2> /dev/null
 mkdir -p /data
 chown $executer:$executer /data
 
-sed -i "s/ubuntu/$executer/g" roles/emmetog.jenkins/defaults/main.yml
 
 if [ ! -f /etc/default/jenkins ]; then
 	touch /etc/default/jenkins
@@ -38,4 +34,6 @@ if [ ! -d venv ]; then
 fi
 source venv/bin/activate
 pip install -r requirements.txt
+ansible-galaxy install --roles-path ./roles emmetog.jenkins
+sed -i "s/ubuntu/$executer/g" roles/emmetog.jenkins/defaults/main.yml
 ansible-playbook deploy-jenkins.yml
